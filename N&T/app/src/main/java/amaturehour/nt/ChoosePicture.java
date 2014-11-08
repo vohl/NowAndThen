cb3291d7ef3023e9d2b283e0c4be7fbffa354ef6
Andrew Vohl <vohl@Andrews-MacBook-Pro.local>
2014-11-08 14:38:05 -0600

    New Version called N&T

---
+++ N&T/app/src/main/java/amaturehour/nt/ChoosePicture.java
@@ -0,0 +1,46 @@
+package amaturehour.nt;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class ChoosePicture extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_picture);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_choose_picture, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void goToCamera(View view){
+        Intent camera_intent = new Intent(this, CustomCamera.class);
+        startActivity(camera_intent);
+    }
+}
