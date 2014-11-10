35d5fb5a874d7821556d9c6804564071afdceecf
Andrew Vohl <vohl@Andrews-MacBook-Pro.local>
2014-11-09 20:34:54 -0600

    camera is working but doesn't traverse to the next activity correctly Need to test the picture sent by choosePicture activity

--- N&T/app/src/main/java/amaturehour/nt/ChoosePicture.java
+++ N&T/app/src/main/java/amaturehour/nt/ChoosePicture.java
@@ -5,17 +5,72 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
 
 
 public class ChoosePicture extends Activity {
+
+    private static final String TAG = "NowAndThen";
+
+    private static final int READ_REQUEST_CODE = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_picture);
+
+        performFileSearch();
     }
 
+    /**
+     * Fires an intent to spin up the "file chooser" UI and select an image.
+     */
+    public void performFileSearch() {
+
+        // ACTION_GET_CONTENT is the intent to choose a file via the system's file
+        // browser and to simply read/import data.
+        // With this approach, the app imports a copy of the data, such as an image file.
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+
+        // Filter to only show results that can be "opened", such as a
+        // file (as opposed to a list of contacts or timezones)
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Filter to show only images, using the image MIME data type.
+        // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
+        // To search for all documents available via installed storage providers,
+        // it would be "*/*".
+        intent.setType("image/*");
+
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
+        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                Log.i(TAG, "Uri: " + uri.toString());
+                Intent customCameraIntent = new Intent(this, CustomCamera.class);
+                customCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivity(customCameraIntent);
+
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,10 +92,5 @@ public class ChoosePicture extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void goToCamera(View view){
-        Intent camera_intent = new Intent(this, CustomCamera.class);
-        startActivity(camera_intent);
     }
 }
--- N&T/app/src/main/java/amaturehour/nt/CustomCamera.java
+++ N&T/app/src/main/java/amaturehour/nt/CustomCamera.java
@@ -1,24 +1,76 @@
 package amaturehour.nt;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 
 public class CustomCamera extends Activity implements PictureCallback, SurfaceHolder.Callback {
 
     public static final String EXTRA_CAMERA_DATA = "camera_data";
 
+    private Camera mCamera;
+    private ImageView mCameraImage;
+    private SurfaceView mCameraPreview;
+    private byte[] mCameraData;
+    private Boolean mIsCapturing;
+
+    public void captureImage(View view){
+        mCamera.takePicture(null, null, this);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        mCameraImage = (ImageView)findViewById(R.id.camera_image_view);
+        mCameraImage.setVisibility(View.INVISIBLE);
+
+        mCameraPreview = (SurfaceView)findViewById(R.id.preview_view);
+        final SurfaceHolder surfaceHolder = mCameraPreview.getHolder();
+        surfaceHolder.addCallback(this);
+
+        mIsCapturing = true;
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mCamera == null){
+            try{
+                mCamera = Camera.open();
+                mCamera.setDisplayOrientation(90);
+                mCamera.setPreviewDisplay(mCameraPreview.getHolder());
+                if(mIsCapturing){
+                    mCamera.startPreview();
+                }
+            }
+            catch(Exception e){
+                Toast.makeText(this, "Unable to open camera.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mCamera != null){
+            mCamera.release();
+            mCamera = null;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,5 +92,36 @@ public class CustomCamera extends Activity implements PictureCallback, SurfaceHo
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPictureTaken(byte[] data, Camera camera) {
+        mCameraData = data;
+        Intent edit_intent = new Intent(this, EditPicture.class);
+        edit_intent.putExtra(EXTRA_CAMERA_DATA, mCameraData);
+        startActivity(edit_intent);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if(mCamera != null){
+            try{
+                mCamera.setPreviewDisplay(holder);
+                if(mIsCapturing){
+                    mCamera.startPreview();
+                }
+            }
+            catch(IOException e){
+                Toast.makeText(CustomCamera.this, "Unable to start camera preview", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
     }
 }
